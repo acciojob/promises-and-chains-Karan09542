9afecbcb83cf742b03e1b4b83cafc44fbcbbc979
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-24 20:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function formValidation(e) {
+	e.preventDefault()
+	const age = document.getElementById("age").value.trim();
+	const name = document.getElementById("name").value.trim();
+	if(!age || !name){
+		alert("Please enter valid details.")
+		return
+	}
+	
+
+	const showMessage = (message, delay=4000) => {
+		setTimeout(()=> alert(message), delay)
+	}
+
+	if(age > 18){
+		showMessage("Welcome,. You can vote.")
+	}else {
+		showMessage("Oh sorry Doe. You aren't old enough.")
+	}
+}
